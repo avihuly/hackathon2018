a0ea26895cf1258d92fa122b06ae26548dd3f8c9
DorNaim <DorNaim1994@gmail.com>
2018-05-29 23:35:26 +0300

    Added comment

--- src/main/java/com/johnbryce/talent/hackathon/controller/LoginController.java
+++ src/main/java/com/johnbryce/talent/hackathon/controller/LoginController.java
@@ -15,5 +15,10 @@ public class LoginController {
 	public void login(@RequestBody UserCredentials userCredentials) {
 		// Actual auth implementation is in JWTLoginFilter
 		// here for proper swagger representation
+
+		// will return 200 with an Authorization header.
+		// for example:
+		// Bearer
+		// eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiJkb3IiLCJleHAiOjE1Mjg0ODkyMDh9.5MSThv67bWPiVKjJpV91tz_1oex55nVG1ryW1cuZbLWnFS8l1EJI1FWHpp8lQ9ACMgHLxbY6UjeuC3JL44T1lg
 	}
 }
